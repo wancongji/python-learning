2e50fe09b217f9c2bf246945e993f2a19ef512ab
wcj <531715112@qq.com>
2018-05-08 17:57:34 +0800

    使用while替换

--- lesson/7/斐波那契数列.py
+++ lesson/7/斐波那契数列.py
@@ -1,11 +1,16 @@
 # 斐波那契数列
 x = 1
 y = 1
-for i in range(1,101):
+i = 1
+# for i in range(1,101):
+while True:
     if i < 3:
         print(1)
     else:
         sum = x + y
+        if sum > 100:
+            break
         x = y
         y = sum
         print(sum)
+    i += 1
