b9a65804b31a77f571f5b589c823de7160f9dd21
wcj <531715112@qq.com>
2018-05-14 11:11:45 +0800

    完善素数，增加程序运行效率

--- lesson/8/素数.py
+++ lesson/8/素数.py
@@ -1,8 +1,11 @@
-# import math
+import math
 lst = [2]
 for i in range(2,100):
     for j in lst:
         if i % j == 0:
+            break
+        if j >= math.ceil(i**0.5):
+            lst.append(i)
             break
     else:
         lst.append(i)
