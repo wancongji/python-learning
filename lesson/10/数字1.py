16465874a0d0bdd20e90e66d4e45b68a889de271
wcj <531715112@qq.com>
2018-05-17 17:24:06 +0800

    完善习题

--- lesson/10/数字1.py
+++ lesson/10/数字1.py
@@ -1,14 +1,39 @@
 while True:
     nums = input("请输入一个数字：").strip().lstrip('0')
     if nums.isdigit():
-        nums = int(nums)    # 也可用于消除开头的零
+        # nums = int(nums)    # 也可用于消除开头的零
         break
     else:
         print("输入错误，请输入数字!")
 
-print("这是个{}位数".format(len(nums)))
+# print("这是个{}位数".format(len(nums)))
+# for i in nums:
+#     print("数字{0},出现过{1}次".format(i,nums.count(i)))
+
+# 判断0-9的数字在字符串中出现的次数，每一次迭代都是用count
+counter = [0]*10
+for i in range(10):
+    counter[i] = nums.count(str(i))
+    if counter[i]:
+        print("The count of {} is {}".format(i,counter[i]))
+
+# 迭代字符串本身的字符
+counter = [0]*10
 for i in nums:
-    print("数字{0},出现过{1}次".format(i,nums.count(i)))
+    i = int(i)
+    if counter[i] == 0:
+        counter[i] = nums.count(str(i))
+        print("The count of {} is {}".format(i,counter[i]))
+
+# 最简复杂度
+counter = [0]*10
+for i in nums:
+    i = int(i)
+    counter[i] += 1
+
+for i in range(len(counter)):
+    if counter[i]:
+        print("The count of {} is {}".format(i,counter[i]))
 
 lst = list(nums)
 lst.reverse()
--- lesson/10/数字2.py
+++ lesson/10/数字2.py
@@ -1,1 +1,27 @@
-nums = input(">>>").strip().lstrip()
+nums = []
+while len(nums) < 5:
+    num = input("Please input a number:").strip().lstrip('0')
+    if not num.isdigit():
+        continue
+    print("The length of {} is {}".format(num,len(nums)+1))
+    nums.append(num)
+print(nums)
+
+# sort方法排序
+lst = nums.copy()
+lst.sort()
+print(lst)
+
+# 冒泡法排序
+for i in range(len(nums)):
+    flag = False
+    for j in range(len(nums)-i-1):
+        if nums[j] > nums[j+1]:
+            tmp = nums[j+1]
+            nums[j+1] = nums[j]
+            nums[j] = tmp
+            flag = True
+    if not flag:
+        break
+print(nums)
+    
