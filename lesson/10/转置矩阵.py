ec2d0e6ab3de6be9be72318b501fd881467e90e0
wcj <531715112@qq.com>
2018-05-19 16:06:14 +0800

    添加不对称矩阵解法

--- lesson/10/转置矩阵.py
+++ lesson/10/转置矩阵.py
@@ -1,7 +1,23 @@
 lst = [[1,2,3],[4,5,6],[7,8,9]]
 for i in range(len(lst)):
-    for j in range(len(lst[i])):
-        lst[i][j] = lst[j][i]
+    for j in range(i):
+        lst[i][j],lst[j][i] = lst[j][i],lst[i][j]
         # print(lst[j][i],end=' ') 
     # print()
 print(lst)
+
+# 不对称矩阵
+matrix = [[1,2,3],[4,5,6]]
+# matrix = [[1,4],[2,5],[3,6]]
+
+# 一次性开辟目标矩阵的目标空间
+tm = [0] * len(matrix[0])
+for i in range(len(tm)):
+    tm[i] = [0] * len(matrix)
+print(tm)
+
+for x in range(len(tm)):
+    for j in range(len(tm[x])):
+        tm[x][j] = matrix[j][x]
+
+print(tm)
