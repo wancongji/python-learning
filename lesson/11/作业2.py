f096ce4e31c7a0e0b59c53c5d788afcd38bab2e6
wcj <531715112@qq.com>
2018-05-25 15:17:26 +0800

    骚操作

--- lesson/11/作业2.py
+++ lesson/11/作业2.py
@@ -1,16 +1,17 @@
 import random
 d = {}
-nums = []
-for i in range(10):
-    # nums.append(random.randint(-1000,1000))
-    nums.append(random.randint(0,10))
+# nums = []
+# for _ in range(10):
+#     nums.append(random.randint(0,10))
+nums = [random.randint(0,10) for _ in range(10)]
 print(nums)
 
 for j in nums:
-    if j not in d.keys():
-        d[j] = 1
-    else:
-        d[j] += 1
+    # if j not in d.keys():
+    #     d[j] = 1
+    # else:
+    #     d[j] += 1
+    d[j] = 1 if j not in d.keys() else d[j]+1
 print(d)    
 
 d1 = sorted(d.items())
