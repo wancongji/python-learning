952fe5cb14004dcd9de4db78690fec23f4dbfdb7
wcj <531715112@qq.com>
2018-06-05 13:49:06 +0800

    使用了pycharm自带的调整格式的工具

--- lesson/13/课堂练习.py
+++ lesson/13/课堂练习.py
@@ -6,26 +6,26 @@
 
 # nums = input('Please input some numbers(e.g:1,2,3,...): ').strip().split(',')
 # print('maximum is {}，minimum is {}'.format(*fn(*nums)))
-        
+
 # 编写一个函数，接受一个参数n，n为正整数，左右两种打印方式。要求数字必须对齐
 def triangle_print(n):
-    for i in range(1,n+1):
-        for j in range(n,0,-1):
+    for i in range(1, n + 1):
+        for j in range(n, 0, -1):
             if i < j:
-                print(' ' * len(str(j)),end=' ')
+                print(' ' * len(str(j)), end=' ')
             else:
                 print(j, end=' ')
 
         print()
 
+
 def trangle_print2(n):
-    tail = ' '.join([str(i) for i in range(n,0,-1)])
+    tail = ' '.join([str(i) for i in range(n, 0, -1)])
     width = len(tail)
-    for i in range(1,n+1):
-        print('{:>{}}'.format(' '.join([str(j) for j in range(i,0,-1)]), width))
+    for i in range(1, n + 1):
+        print('{:>{}}'.format(' '.join([str(j) for j in range(i, 0, -1)]), width))
 
 
 n = int(input('Please input a number: ').strip())
 # triangle_print(n)
 trangle_print2(n)
-
--- lesson/15/test.py
+++ lesson/15/test.py
@@ -1,17 +1,21 @@
 def foo1(b, b1=3):
     print("foo1 called", b, b1)
 
+
 def foo2(c):
     foo3(c)
     print("foo2 called", c)
 
+
 def foo3(d):
     print("foo3 called", d)
 
+
 def main():
     print("main called")
-    foo1(100,101)
+    foo1(100, 101)
     foo2(200)
     print("main ending")
+
 
 main()
