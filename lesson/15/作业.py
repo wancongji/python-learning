dd6ed1373ba3d52b7f7e97a71bd05b9dae12a0ab
wcj <531715112@qq.com>
2018-06-12 18:00:03 +0800

    装饰器2

--- lesson/15/作业.py
+++ lesson/15/作业.py
@@ -1,20 +1,33 @@
 # 字典扁平化
 d = {'a': {'b': 1, 'c': 2}, 'd': {'e': 3, 'f': {'g': 4}}}
-d1 = {}
-lst = []
 
 
-def flat(prefix='', **kwargs):
-    for k, v in kwargs.items():
+def flat(src, dst=None, prefix=''):
+    if dst is None:
+        dst = {}
+    for k, v in src.items():
         # print(k, v)
         if isinstance(v, (list, tuple, dict, set)):
-            flat(prefix=prefix+k+'.',**v)
+            flat(v, dst, prefix=prefix + k + '.')
         else:
-            d1[prefix+k] = v
+            dst[prefix + k] = v
+    return dst
 
 
-flat(**d)
-print(d1)
+def flat2(src):
+    def _flat(src, dst=None, prefix=''):
+        for k, v in src.items():
+            if isinstance(v, (list, tuple, dict, set)):
+                _flat(v, dst, prefix=prefix + k + '.')
+            else:
+                dst[prefix + k] = v
 
+    dst = {}
+    _flat(src, dst)
+    return dst
+
+
+print(flat(d))
+print(flat2(d))
 
 # 实现base64编码
