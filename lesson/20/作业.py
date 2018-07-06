19cca50cc642408585b5a7805be0470a55af3a66
wcj <531715112@qq.com>
2018-07-06 11:22:06 +0800

    ls终版

--- lesson/20/作业.py
+++ lesson/20/作业.py
@@ -1,46 +1,83 @@
 from pathlib import Path
 import argparse
-
-
-def showdir(path: str = '.', all=False, detail=False, human=False):
-    p = Path(path)
-    for file in p.iterdir():
-        print(type(file.name))
-        if not all and file.name.startswith('.'):
-            continue
-
-        # -l
-        if detail:
-            st = file.stat()
-            yield (st.st_mode, st.st_nlink, st.st_uid, st.st_gid, st.st_size,
-                   st.st_atime, file.name)
-            pass
-        else:
-            yield file.name
-
+from datetime import datetime
 
 parser = argparse.ArgumentParser(prog='ls', add_help=True, description='list all files')  # 构造解析器
-parser.add_argument('path', nargs='?', default='.', help='path help')  # 位置参数
-parser.add_argument('-l', action='store_true')
+parser.add_argument('path', nargs='?', default='.', help='path help')  # 位置参数，可省略，缺省值为'.'
+parser.add_argument('-l', action='store_true')  # 可选参数
 parser.add_argument('-a', '--all', action='store_true')
-# parser.add_argument("square", type=int, help="display a square of a given number")
-# parser.add_argument('x', type=int, help="the base")
-# parser.add_argument('y', type=int, help="the exponent")
-# parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
-#
-# answer = args.square ** 2
-# answer2 = args.x * args.y
-# if args.verbose:
-#     print("The square of {} is {}".format(args.square, answer))
-# else:
-#     print(answer)
+
+
+
+# parser.print_help()
+
+
+def listdir(path, all=False, detail=False):
+    def _getfiletype(f: Path):
+        """
+        获取文件类型
+        :param f:
+        :return:
+        """
+        if f.is_dir():
+            return 'd'
+        elif f.is_block_device():
+            return 'b'
+        elif f.is_char_device():
+            return 'c'
+        elif f.is_symlink():
+            return 'l'
+        elif f.is_socket():
+            return 's'
+        else:
+            return '-'
+
+    def _getmodestr(mode: int):
+        """
+        将字符串转化为rwx形式
+        :param mode:
+        :return:
+        """
+        modelist = ['r', 'w', 'x', 'r', 'w', 'x', 'r', 'w', 'x']
+        m = mode & 0o777
+        # return m, bin(m)
+        mstr = ''
+        for i, v in enumerate(bin(m)[2:]):
+            if v == '1':
+                mstr += modelist[i]
+            else:
+                mstr += '-'
+        return mstr
+
+    print(_getmodestr(int('777')))
+
+    def _listdir(path, all=False, detail=False):
+        """
+        列出文件目录
+        :param detail:
+        :param path:
+        :param all:
+        :return:
+        """
+        p = Path(path)
+        for i in p.iterdir():
+            if not all and i.name.startswith('.'):  # 当没给all时，不提供隐藏文件
+                continue
+            if not detail:
+                yield (i.name)
+            else:
+                # -rwxr-xr-x. 1 root root 154 3月  14 19:27 parameter.sh
+                stat = p.stat()
+                d = _getfiletype(p)
+                mode = oct(p.stat().st_mode)[-3:]
+                atime = datetime.fromtimestamp(stat.st_atime).strftime('%Y-%m-%d %H:%M:%S')
+                yield (d, _getmodestr(int(mode)), stat.st_uid, stat.st_gid, stat.st_size, atime, i.name)
+
+    # 排序
+    yield from sorted(_listdir(path, all, detail), key=lambda x: x[len(x) - 1])
+
 
 if __name__ == '__main__':
-    args = parser.parse_args(('G:\\',))
-    parser.print_help()
-
-    print(args)
-    print(args.path, args.l, args.all)
-
-    for file in showdir(args.path):
-        print(file)
+    args = parser.parse_args()  # 分析参数，同时传入可迭代的参数
+    files = listdir(args.path, args.all, args.l)
+    print(list(files))
